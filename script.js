5ac512572cb963a4f6644e1a888325d863d65466
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-25 12:28:40 +0500

    new design update start

---
+++ script.js
@@ -0,0 +1,6 @@
+window.onload = ()=>{
+    setTimeout(()=>{
+        document.querySelector(".spinnerContainer").style.display = "none";
+        document.querySelector(".container").style.display = "block";
+    },1000) //3 sec normal decrrease for developmenr
+}

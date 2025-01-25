92c69b92621a4981ea125b51fd1abe15af5ce09b
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-25 14:01:45 +0500

    cursor changes

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ window.onload = ()=>{
         document.querySelector(".container").style.display = "block";
     },1000) //3 sec normal decrrease for developmenr
 }
+
+// Create the custom cursor element
+const cursor = document.createElement('div');
+cursor.classList.add('cursor');
+document.body.appendChild(cursor);
+
+// Update cursor position on mousemove
+document.addEventListener('mousemove', (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    cursor.style.left = `${x}px`;
+    cursor.style.top = `${y}px`;
+});

68d5bf8a2088b77812b458482dddddfcc4fd4a47
RafayMemon01 <rafayrashid200611@gmail.com>
2025-01-25 13:02:29 +0500

    small changes

---
+++ typing.js
@@ -0,0 +1,26 @@
+let different = ["MERN STACK DEVELOPER", "WEBSITE DEVELOPER", "WORDPRESS WEBSITE DEVELOPER", "Learner", "Coder"];
+
+let index = 0; 
+let heroTextElement = document.getElementById("heroText");
+
+function typeText(text, callback) {
+    let currentIndex = 0;
+    heroTextElement.innerHTML = ""; 
+    let typingInterval = setInterval(function() {
+        heroTextElement.innerHTML += text[currentIndex];
+        currentIndex++;
+        if (currentIndex === text.length) {
+            clearInterval(typingInterval);
+            setTimeout(callback, 2000); 
+        }
+    }, 100); 
+}
+
+function changeText() {
+    typeText(different[index], function() {
+        index = (index + 1) % different.length; 
+        changeText(); 
+    });
+}
+
+changeText();
